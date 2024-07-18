fc65d93d3392c6c45ce8f4eaaf664dfd6d6be18f
Sukhmanpreet Singh <sukhmandev4@gmail.com>
2024-07-17 20:17:49 -0700

    Assignment 2 donme

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// global variables
+// global variables--------------------------------------------------------
 
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
-let checkersBoard = [
+let board = [
     ["", "red", "", "red", "", "red", "", "red"],
     ["red", "", "red", "", "red", "", "red", ""],
     ["", "red", "", "red", "", "red", "", "red"],
@@ -14,9 +14,10 @@ let checkersBoard = [
     ["gray", "", "gray", "", "gray", "", "gray", ""]
 ];
 
-// event handlers
-drawBoard();
-drawPeices();
+// event handlers-----------------------------------------------------------
+generateBoard(); // change color to Peice objects
+drawBoard(); // draw background of board
+drawPeices(); // draw peices of red and grey
 
 canvas.onclick = function (event) {
     let x = event.offsetX;
@@ -25,10 +26,56 @@ canvas.onclick = function (event) {
     let row = Math.floor(y / 100);
     let col = Math.floor(x / 100);
 
-    alert(checkersBoard[row][col]);    
+    if (board[row][col] != "")
+    {
+        let selectedPeice = getSelectedPeice();
+        if (selectedPeice != null)
+        {
+            selectedPeice.isClicked = false;
+        }
+        else 
+        {
+            board[row][col].isClicked = !board[row][col].isClicked;
+        }
+    } 
+    drawBoard();
+    drawPeices();
 }
 
-// functions
+// Constructors---------------------------------------------------------------
+function Peice(row, col, color, isClicked, isKing)
+{
+    this.row = row;
+    this.col = col;
+    this.color = color;
+    this.isClicked = isClicked;
+    this.isKing = isKing;
+
+    this.draw = function () {
+        let x = col * 100 + 50;
+        let y = row * 100 + 50;
+        
+        drawCircle(x, y, 35, this.color);  
+    };
+
+    this.checkKing = function () {
+        if (this.color == "red" && this.row == 7)
+        {
+            this.isKing = true;
+        }
+
+        if (this.color == "grey" && this.row == 0)
+        {
+            this.isKing = true;
+        }
+    };
+
+    this.move = function (newRow, newCol) {
+
+    };
+}
+
+// functions--------------------------------------------------------
 function drawBoard() 
 {
     let rows = 8;
@@ -50,30 +97,66 @@ function drawBoard()
                 ctx.fillStyle = "black";
                 ctx.fillRect(i*100, j*100, cellSide, cellSide);
             }
-        }
-            
+        }   
     }
 }
 
 function drawPeices()
 {
-    let rows = checkersBoard.length;
-    let cols = checkersBoard[0].length;
+    let rows = board.length;
+    let cols = board[0].length;
 
     for (let i = 0; i < rows; i++) 
     {
         for (let j = 0; j < cols; j++)
         {
-            if (checkersBoard[i][j] != "")
+            let peice = board[i][j];
+            if (peice != "")
             {
-                let x = j * 100 + 50;
-                let y = i * 100 + 50;
-                let color = checkersBoard[i][j];
-                
-                drawCircle(x, y, 35, color);   
-            }
+                if (peice.isClicked)
+                {
+                    let x = j * 100 + 50;
+                    let y = i * 100 + 50;
+                    
+                    drawCircle(x, y, 40, "yellow");  
+                }
+                peice.draw();            
+            }          
         }
-            
+    }
+}
+
+function getSelectedPeice()
+{
+    let rows = board.length;
+    let cols = board[0].length;
+
+    for (let i = 0; i < rows; i++) 
+    {
+        for (let j = 0; j < cols; j++)
+        {
+            if (board[i][j].isClicked)
+            {
+                return board[i][j];          
+            }          
+        }
+    }
+
+    return null;
+}
+
+// Helper Functions----------------------------------------------
+function generateBoard()
+{
+    for (let i = 0; i < 8; i++)
+    {
+        for (let j = 0; j < 8; j++)
+        {
+            if (board[i][j] != "")
+            {
+                board[i][j] = new Peice(i, j, board[i][j], false, false);
+            }
+        }  
     }
 }
 
